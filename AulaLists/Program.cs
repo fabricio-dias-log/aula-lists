c82b1970eaa5e256e09210b843b1bd7ddf1665d9
Fabricio Dias <fabricio270909@gmail.com>
2024-06-26 14:22:29 -0300

    feat: using list find

--- AulaLists/Program.cs
+++ AulaLists/Program.cs
@@ -19,5 +19,21 @@ class Program
         }
 
         Console.WriteLine($"list count: {list.Count}");
+
+        //Lógica em método
+        string s1 = list.Find(FirstA); //espera um predicado ou lógica como parametro para encontrar item
+        Console.WriteLine($"Primeiro com a letra 'A': {s1}");
+        
+        //expressão lâmbida
+        string s2 = list.Find(x => x[0] == 'A'); //espera um predicado ou lógica como parametro para encontrar item
+        Console.WriteLine($"Primeiro com a letra 'A': {s1}");
+
+        string s3 = list.FindLast(x => x[0] == 'A'); //retorna ultimo item de acordo com a lógica
+        Console.WriteLine($"Último com a letra 'A': {s1}");
+    }
+
+    static bool FirstA(string list)
+    {
+        return list[0] == 'A';
     }
 }
